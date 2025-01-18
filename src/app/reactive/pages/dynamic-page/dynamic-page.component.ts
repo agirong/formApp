06e632923d191200d99e41ac32c7644aceff0326
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 14:48:22 -0600

    Formularios Dinámicos.

--- src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
@@ -6,4 +7,26 @@ import { Component } from '@angular/core';
 })
 export class DynamicPageComponent {
 
+  constructor(private fb:FormBuilder){}
+
+  public myForm: FormGroup = this.fb.group({
+    name:['',[Validators.required,Validators.minLength(3)]],
+    favoriteGames: this.fb.array([
+      ['Metal Gear',Validators.required],
+      ['Death Stranding',Validators.required],
+    ])
+  })
+
+  get favoriteGames(){
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+
+  onSubmit():void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+    console.log(this.myForm.value)
+    this.myForm.reset();
+  }
 }
