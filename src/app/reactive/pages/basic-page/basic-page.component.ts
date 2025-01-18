018233ee0e910a7991330ca93cacb23db7d4d712
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 11:21:32 -0600

    Forms Validators. Marcar requeridos, Longitud minima. Ver estados: Valid, Prisitine y Touched

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -15,13 +15,16 @@ export class BasicPageComponent {
   // })
 
   constructor(private fb:FormBuilder){}
+
   public myForm:FormGroup = this.fb.group({
-    name: [''],
-    price:[0],
-    inStorage:[0],
+    name: ['',[Validators.required, Validators.minLength(3)]],
+    price:[0, [Validators.required, Validators.min(0)]],
+    inStorage:[0, [Validators.required, Validators.min(0)]],
   })
 
   onSave():void{
+    if(this.myForm.invalid) return;
+
     console.log(this.myForm.value)
   }
 }
