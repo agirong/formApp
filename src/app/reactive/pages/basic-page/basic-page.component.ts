5ce4f8bfdfa9b77767a6518d1c83663b447851ee
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 10:55:51 -0600

    Crear un fromulario reactivo y conectarlo con los controles en el html y obtener sus valores.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-page',
@@ -6,5 +7,21 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class BasicPageComponent {
+  //Importar ReactiveFormsModule para usar sus directivas.
+  // public myForm: FormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  // })
 
+  constructor(private fb:FormBuilder){}
+  public myForm:FormGroup = this.fb.group({
+    name: [''],
+    price:[0],
+    inStorage:[0],
+  })
+
+  onSave():void{
+    console.log(this.myForm.value)
+  }
 }
