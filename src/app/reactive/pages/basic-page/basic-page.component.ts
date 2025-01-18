3bec8775ea31e57385a5cf34a3a8bf086dc961c6
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 12:06:52 -0600

    Métodos para obtener las validaciones incumplidas y mostrar mensajes al usuario basadas en estas validaciones.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -31,8 +31,34 @@ export class BasicPageComponent implements OnInit{
     inStorage:[0, [Validators.required, Validators.min(0)]],
   })
 
+  //Funcion para validar si hay errores en los campos
+  isValidField(field:string): boolean | null{
+    return this.myForm.controls[field].errors
+    && this.myForm.controls[field].touched;
+  }
+
+  getFiedlError(field:string):string | null{
+    if(!this.myForm.controls[field]) return null;
+
+    const errors = this.myForm.controls[field].errors || {}
+
+    for(const key of Object.keys(errors)){
+      switch(key){
+        case 'required':
+          return 'Este campo es requerido';
+        case 'minlength':
+          return `Mínimo ${errors['minlength'].requiredLength} caracteres`;
+      }
+    }
+
+    return null;
+  }
+
   onSave():void{
-    if(this.myForm.invalid) return;
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
 
     console.log(this.myForm.value)
 
