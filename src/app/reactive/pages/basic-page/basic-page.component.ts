86a64eb92d970b80b1b01d5be9bded98f96728d7
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 11:31:46 -0600

    Establecer y Re - Establecer los valores en un Form reactive.

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+const rtx5090 ={
+  name:'RTX 5090',
+  price:809809,
+  inStorage:2
+}
 
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit{
   //Importar ReactiveFormsModule para usar sus directivas.
   // public myForm: FormGroup = new FormGroup({
   //   name: new FormControl(''),
@@ -15,6 +21,9 @@ export class BasicPageComponent {
   // })
 
   constructor(private fb:FormBuilder){}
+  ngOnInit(): void {
+    this.myForm.reset(rtx5090);
+  }
 
   public myForm:FormGroup = this.fb.group({
     name: ['',[Validators.required, Validators.minLength(3)]],
@@ -26,5 +35,14 @@ export class BasicPageComponent {
     if(this.myForm.invalid) return;
 
     console.log(this.myForm.value)
+
+    //Reestablecer los valores al formulario
+    this.myForm.reset(
+      {
+        name:'Nombre del producto',
+        price:0,
+        inStorage:0,
+      }
+    )
   }
 }
