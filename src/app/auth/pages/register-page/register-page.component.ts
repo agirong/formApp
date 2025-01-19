27d4cbb8a960d9c11a8ca2d818bb854d7e586b64
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-18 18:04:37 -0600

    Formulario de registro crear el formulario y conectarlo con el html

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
 export class RegisterPageComponent {
+
+  constructor(private fb: FormBuilder){}
+
+  public myForm:FormGroup = this.fb.group({
+    name:['',[Validators.required]],
+    email:['',[Validators.required]],
+    username:['',[Validators.required]],
+    password:['',[Validators.required, Validators.minLength(6)]],
+    password2:['',[Validators.required]],
+  })
+
+
+  isValidField(field:string){
+    // TODO: Obterner validación desde un servicio.
+  }
+
+  //ngSubmit
+  onSubmit():void{
+    if(this.myForm.invalid){
+      this.myForm.markAllAsTouched();
+      return;
+    }
+  }
 
 }
