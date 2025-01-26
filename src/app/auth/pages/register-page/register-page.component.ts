90ebb31e2c8ba62317b1603133e9fb6205839a3a
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-26 17:09:15 -0600

    Validado para el correo.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -13,11 +13,13 @@ export class RegisterPageComponent {
   constructor(
     private fb: FormBuilder,
     private validatorService: ValidatorService,
+    private emailValidator: EmailValidator,
   ){}
 
   public myForm:FormGroup = this.fb.group({
     name:['',[Validators.required,Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)], new EmailValidator()],
+    // email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)], new EmailValidator()],
+    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)], this.emailValidator],
     username:['',[Validators.required, this.validatorService.canBeStrider]],
     password:['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]],
@@ -25,16 +27,11 @@ export class RegisterPageComponent {
 
 
   isValidField(field:string){
-    this.validatorService.isValidField(this.myForm,field);
+    return this.validatorService.isValidField(this.myForm,field);
   }
 
   //ngSubmit
   onSubmit():void{
-    console.log('onSubmit')
-    if(this.myForm.invalid){
-      this.myForm.markAllAsTouched();
-      return;
-    }
-    console.log('onSubmit')
+    this.myForm.markAllAsTouched();
   }
 }
