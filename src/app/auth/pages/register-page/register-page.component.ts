a17428b3ea7afd6de7f3fe205b59d25337611844
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-26 16:39:31 -0600

    Validaciones Asyncronas.

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorService } from '../../../shared/service/validator.service';
+import { EmailValidator } from '../../../shared/service/email.validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -16,7 +17,7 @@ export class RegisterPageComponent {
 
   public myForm:FormGroup = this.fb.group({
     name:['',[Validators.required,Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
-    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)], new EmailValidator()],
     username:['',[Validators.required, this.validatorService.canBeStrider]],
     password:['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]],
--- src/app/shared/service/validator.service.ts
+++ src/app/shared/service/validator.service.ts
@@ -3,7 +3,6 @@ import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
 
 @Injectable({providedIn: 'root'})
 export class ValidatorService {
-  constructor() { }
   public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
   public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
 
