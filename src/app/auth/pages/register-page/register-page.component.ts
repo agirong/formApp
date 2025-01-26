7f1e5fbfb0431a90e0aa064827d7929829e57d15
Aaron Obed Girón <aogarrido.g@gmail.com>
2025-01-26 16:27:08 -0600

    Validator Service

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import * as customValidators from '../../../shared/validators/validators';
+// import * as customValidators from '../../../shared/validators/validators';
+import { ValidatorService } from '../../../shared/service/validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -8,27 +9,31 @@ import * as customValidators from '../../../shared/validators/validators';
 })
 export class RegisterPageComponent {
 
-  constructor(private fb: FormBuilder){}
+  constructor(
+    private fb: FormBuilder,
+    private validatorService: ValidatorService,
+  ){}
 
   public myForm:FormGroup = this.fb.group({
-    name:['',[Validators.required,Validators.pattern(customValidators.firstNameAndLastnamePattern)]],
-    email:['',[Validators.required, Validators.pattern(customValidators.emailPattern)]],
-    username:['',[Validators.required, customValidators.canBeStrider]],
+    name:['',[Validators.required,Validators.pattern(this.validatorService.firstNameAndLastnamePattern)]],
+    email:['',[Validators.required, Validators.pattern(this.validatorService.emailPattern)]],
+    username:['',[Validators.required, this.validatorService.canBeStrider]],
     password:['',[Validators.required, Validators.minLength(6)]],
     password2:['',[Validators.required]],
   })
 
 
   isValidField(field:string){
-    // TODO: Obterner validación desde un servicio.
+    this.validatorService.isValidField(this.myForm,field);
   }
 
   //ngSubmit
   onSubmit():void{
+    console.log('onSubmit')
     if(this.myForm.invalid){
       this.myForm.markAllAsTouched();
       return;
     }
+    console.log('onSubmit')
   }
-
 }
---
+++ src/app/shared/service/validator.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+import { FormControl, FormGroup, ValidationErrors } from '@angular/forms';
+
+@Injectable({providedIn: 'root'})
+export class ValidatorService {
+  constructor() { }
+  public firstNameAndLastnamePattern: string = '([a-zA-Z]+) ([a-zA-Z]+)';
+  public emailPattern: string = "^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$";
+
+  //Regresar un mensaje de error
+  public canBeStrider= (control: FormControl):ValidationErrors | null =>{
+
+    const user:string = control.value.trim().toLowerCase();
+
+    if(user === 'strider'){
+      return{
+        noStrider:true,
+      }
+    }
+    return null;
+  }
+
+  public isValidField(form:FormGroup, field: string){
+    return form.controls[field].errors && form.controls[field].touched;
+  }
+
+}
